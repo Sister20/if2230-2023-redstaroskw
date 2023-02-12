a135a334fd1951da74e1c1ecfad782a6f93757d1
IceTeaXXD <13521024@std.stei.itb.ac.id>
2023-02-12 23:29:39 +0700

    Update framebuffer and kernel to show output

--- src/framebuffer.c
+++ src/framebuffer.c
@@ -3,20 +3,57 @@
 #include "lib-header/stdmem.h"
 #include "lib-header/portio.h"
 
+void port_byte_out(uint16_t port, uint8_t data) {
+    /*
+    I.S. Menerima port dan data
+    F.S. Mengirimkan data ke port
+    */
+    asm volatile ("outb %0, %1" : : "a"(data), "Nd"(port));
+    // "a" (data) -> data akan dimasukkan ke register
+    // "Nd" (port) -> port akan dimasukkan ke register
+    // "outb" -> untuk menuliskan 'data' ke 'port'
+    // "%0" -> data akan dimasukkan ke register %eax
+    // "%1" -> port akan dimasukkan ke register %edx
+    // volatile -> menandakan bahwa kode assembly memiliki efek dimana akan melakukan writing pada I/O port supaya compiler tidak mengoptimasi kode assembly tersebut
+}
+
 void framebuffer_set_cursor(uint8_t r, uint8_t c) {
-    // TODO : Implement
-    MEMORY_FRAMEBUFFER = MEMORY_FRAMEBUFFER + (y*r + r);
+    /*
+    I.S. Menerima r sebagai baris cursor dan c sebagai kolom cursor
+    F.S. Mengubah posisi cursor ke baris r dan kolom c
+    */
+    uint16_t pos = r * 80 + c; // Menghitung posisi cursor
+    // Mengirimkan posisi cursor ke port CURSOR_PORT_CMD dan CURSOR_PORT_DATA
+    port_byte_out(CURSOR_PORT_CMD, 0x0F);
+    port_byte_out(CURSOR_PORT_DATA, (uint8_t) (pos & 0xFF));
+    port_byte_out(CURSOR_PORT_CMD, 0x0E);
+    port_byte_out(CURSOR_PORT_DATA, (uint8_t) ((pos >> 8) & 0xFF));
 }
 
 void framebuffer_write(uint8_t row, uint8_t col, char c, uint8_t fg, uint8_t bg) {
-    // TODO : Implement
-    // Add the attribute
-    uint8_t attr = (bg << 2) | (fg & 0x0F);
-    // Set the position
-    framebuffer_set_cursor(row, col) = c | (attr << 4);
+    /*
+    I.S. Menerima :
+            row = baris dari karakter c
+            col = kolom dari karakter c
+            c = karakter yang akan ditulis
+            fg = warna karakter
+            bg = warna background
+    F.S. Menulis karakter c ke framebuffer dengan warna fg dan warna background bg
+    */
+    uint16_t pos = row * 80 + col; // Menghitung posisi karakter c
+    uint8_t color = (bg << 4) | fg; // Memberi warna dan warna background pada karakter c
+    uint16_t *fb = (uint16_t *) MEMORY_FRAMEBUFFER; // Mengambil alamat memory framebuffer
+    fb[pos] = (color << 8) | c; // Menulis karakter c ke framebuffer dengan warna color (shift informasi color ke HIGH BYTE)
 }
 
 void framebuffer_clear(void) {
-    // TODO : Implement
-    /* HIPOTESIS: kembalikan ke MEMORY_FRAMEBUFFER sebelumnya*/
+    /*
+    I.S. -
+    F.S. Menghapus semua karakter yang ada di framebuffer
+    */
+    uint16_t *fb = (uint16_t *) MEMORY_FRAMEBUFFER; // Mengambil alamat memory framebuffer
+    // Looping untuk menghapus semua karakter yang ada di framebuffer
+    for (uint16_t i = 0; i < 80 * 25; i++) {
+        fb[i] = (0x07 << 8) | 0x00;
+    }
 }
--- src/kernel.c
+++ src/kernel.c
@@ -5,9 +5,46 @@
 #include "lib-header/framebuffer.h"
 #include "lib-header/kernel_loader.h"
 
+// void kernel_setup(void) {
+//     uint32_t a;
+//     uint32_t volatile b = 0x0000BABE;
+//     __asm__("mov $0xCAFE0000, %0" : "=r"(a));
+//     while (TRUE) b += 1;
+// }
+
 void kernel_setup(void) {
-    uint32_t a;
-    uint32_t volatile b = 0x0000BABE;
-    __asm__("mov $0xCAFE0000, %0" : "=r"(a));
-    while (TRUE) b += 1;
+    framebuffer_clear();
+    framebuffer_write(3, 8,  'H', 0, 0xF);
+    framebuffer_write(3, 9,  'a', 0, 0xF);
+    framebuffer_write(3, 10, 'i', 0, 0xF);
+    framebuffer_write(3, 11, '!', 0, 0xF);
+    // write hello world!
+    framebuffer_write(4, 8,  'H', 0, 0xF);
+    framebuffer_write(4, 9,  'e', 0, 0xF);
+    framebuffer_write(4, 10, 'l', 0, 0xF);
+    framebuffer_write(4, 11, 'l', 0, 0xF);
+    framebuffer_write(4, 12, 'o', 0, 0xF);
+    framebuffer_write(4, 13, ' ', 0, 0xF);
+    framebuffer_write(4, 14, 'w', 0, 0xF);
+    framebuffer_write(4, 15, 'o', 0, 0xF);
+    framebuffer_write(4, 16, 'r', 0, 0xF);
+    framebuffer_write(4, 17, 'l', 0, 0xF);
+    framebuffer_write(4, 18, 'd', 0, 0xF);
+    framebuffer_write(4, 19, '!', 0, 0xF);
+    // write jason uff uff
+    framebuffer_write(5, 8,  'J', 0, 0xF);
+    framebuffer_write(5, 9,  'a', 0, 0xF);
+    framebuffer_write(5, 10, 's', 0, 0xF);
+    framebuffer_write(5, 11, 'o', 0, 0xF);
+    framebuffer_write(5, 12, 'n', 0, 0xF);
+    framebuffer_write(5, 13, ' ', 0, 0xF);
+    framebuffer_write(5, 14, 'u', 0, 0xF);
+    framebuffer_write(5, 15, 'f', 0, 0xF);
+    framebuffer_write(5, 16, 'f', 0, 0xF);
+    framebuffer_write(5, 17, ' ', 0, 0xF);
+    framebuffer_write(5, 18, 'u', 0, 0xF);
+    framebuffer_write(5, 19, 'f', 0, 0xF);
+    framebuffer_write(5, 20, 'f', 0, 0xF);
+    framebuffer_set_cursor(5, 19);
+    while (TRUE);
 }
