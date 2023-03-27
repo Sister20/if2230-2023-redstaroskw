bd35466d5c4c480837330af3191acfc481304ddf
jasonrivalino <jasonrivalino@gmail.com>
2023-03-27 12:08:43 +0700

    add 3.4 test kernel.c

--- src/kernel.c
+++ src/kernel.c
@@ -7,6 +7,7 @@
 #include "lib-header/idt.h"
 #include "lib-header/interrupt.h"
 #include "lib-header/keyboard.h"
+#include "lib-header/fat32.h"
 
 
 void kernel_setup(void) {
@@ -16,12 +17,55 @@ void kernel_setup(void) {
     framebuffer_clear();
     framebuffer_set_cursor(0, 0);
     // int32_t id = 0;
+    //initialize_filesystem_fat32();
     while (TRUE) 
     {
         keyboard_state_activate();
         // id++;
     }
+
+    struct ClusterBuffer cbuf[5];
+    for (uint32_t i = 0; i < 5; i++)
+        for (uint32_t j = 0; j < CLUSTER_SIZE; j++)
+            cbuf[i].buf[j] = i + 'a';
+
+    struct FAT32DriverRequest request = {
+        .buf                   = cbuf,
+        .name                  = "ikanaide",
+        .ext                   = "uwu",
+        .parent_cluster_number = ROOT_CLUSTER_NUMBER,
+        .buffer_size           = 0,
+    } ;
+
+    write(request);  // Create folder "ikanaide"
+    memcpy(request.name, "kano1\0\0\0", 8);
+    write(request);  // Create folder "kano1"
+    memcpy(request.name, "ikanaide", 8);
+    delete(request); // Delete first folder, thus creating hole in FS
+
+    memcpy(request.name, "daijoubu", 8);
+    request.buffer_size = 5*CLUSTER_SIZE;
+    write(request);  // Create fragmented file "daijoubu"
+
+    struct ClusterBuffer readcbuf;
+    read_clusters(&readcbuf, ROOT_CLUSTER_NUMBER+1, 1); 
+    // If read properly, readcbuf should filled with 'a'
+
+    request.buffer_size = CLUSTER_SIZE;
+    read(request);   // Failed read due not enough buffer size
+    request.buffer_size = 5*CLUSTER_SIZE;
+    read(request);   // Success read on file "daijoubu"
 }
+
+
+
+
+
+
+
+
+
+
 
 
 // void kernel_setup(void) {
