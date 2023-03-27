4bc20caa80bf32791691d1200f68fe4b11a1c247
MHEN2606 <matthew.mahendra@gmail.com>
2023-03-27 22:10:19 +0700

    Setup for Write Delete Read

--- src/fat32.c
+++ src/fat32.c
@@ -116,3 +116,49 @@ void init_directory_table(struct FAT32DirectoryTable *dir_table, char *name, uin
     dir_table->table[2].cluster_high = 0;
     dir_table->table[2].cluster_low = 2;
 }
+
+/**
+ * FAT32 read, read a file from file system.
+ *
+ * @param request All attribute will be used for read, buffer_size will limit reading count
+ * @return Error code: 0 success - 1 not a file - 2 not enough buffer - 3 not found - -1 unknown
+ */
+int8_t read(struct FAT32DriverRequest request)
+{
+
+}
+
+/**
+ * FAT32 write, write a file or folder to file system.
+ *
+ * @param request All attribute will be used for write, buffer_size == 0 then create a folder / directory
+ * @return Error code: 0 success - 1 file/folder already exist - 2 invalid parent cluster - -1 unknown
+ */
+int8_t write(struct FAT32DriverRequest request)
+{
+    if(request.buffer_size == 0)
+    {
+        /* Make a Folder */
+    }
+    else
+    {
+        /* Check for Existing Folder */
+    }
+}
+
+
+/**
+ * FAT32 delete, delete a file or empty directory (only 1 DirectoryEntry) in file system.
+ *
+ * @param request buf and buffer_size is unused
+ * @return Error code: 0 success - 1 not found - 2 folder is not empty - -1 unknown
+ */
+int8_t delete(struct FAT32DriverRequest request)
+{
+    while(request.parent_cluster_number != 0x0FFFFFFF)
+    {
+        /* Karena merupakan linked list, maka ini akan diperlukan
+         * suatu linker
+         */
+    }
+}
