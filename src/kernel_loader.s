3f99af49d32529e21df7d04cbd4c56c245fe22ff
IceTeaXXD <13521024@std.stei.itb.ac.id>
2023-03-28 20:03:52 +0700

    feat: multiboot

--- src/kernel_loader.s
+++ src/kernel_loader.s
@@ -15,14 +15,14 @@ align 4                              ; align at 4 bytes
 kernel_stack:                        ; label points to beginning of memory
     resb KERNEL_STACK_SIZE           ; reserve stack for the kernel
 
-section .text                        ; start of the text (code) section
+section .multiboot                        ; start of the text (code) section
 align 4                              ; the code must be 4 byte aligned
     dd MAGIC_NUMBER                  ; write the magic number to the machine code,
     dd FLAGS                         ; the flags,
     dd CHECKSUM                      ; and the checksum
 
 
-
+section .text
 loader:                                        ; the loader label (defined as entry point in linker script)
     mov  esp, kernel_stack + KERNEL_STACK_SIZE ; setup stack register to proper location
     call kernel_setup
