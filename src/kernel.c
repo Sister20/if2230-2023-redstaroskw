3918494b613403ebfa51243310a8e45040d1ceaf
henryanandsr <13521004@std.stei.itb.ac.id>
2023-03-30 14:13:38 +0700

    fix : write

--- src/kernel.c
+++ src/kernel.c
@@ -8,8 +8,6 @@
 #include "lib-header/interrupt.h"
 #include "lib-header/keyboard.h"
 #include "lib-header/fat32.h"
-
-
 void kernel_setup(void) {
     enter_protected_mode(&_gdt_gdtr);
     pic_remap();
@@ -18,7 +16,7 @@ void kernel_setup(void) {
     framebuffer_clear();
     framebuffer_set_cursor(0, 0);
     initialize_filesystem_fat32();
-    // keyboard_state_activate();
+    keyboard_state_activate();
 
     struct ClusterBuffer cbuf[5];
     for (uint32_t i = 0; i < 5; i++)
@@ -37,40 +35,26 @@ void kernel_setup(void) {
     memcpy(request.name, "kano1\0\0\0", 8);
     write(request);  // Create folder "kano1"
     memcpy(request.name, "ikanaide", 8);
-    // delete(request); // Delete first folder, thus creating hole in FS
+    memcpy(request.ext, "\0\0\0", 3);
+    delete(request); // Delete first folder, thus creating hole in FS
 
     memcpy(request.name, "daijoubu", 8);
     request.buffer_size = 5*CLUSTER_SIZE;
     write(request);  // Create fragmented file "daijoubu"
 
-    // memcpy(request.name, "haihaiha", 8);
-    // write(request);  // Create fragmented file "daijoubu"
-    // struct ClusterBuffer readcbuf;
-    // read_clusters(&readcbuf, ROOT_CLUSTER_NUMBER+1, 1); 
-    // // If read properly, readcbuf should filled with 'a'
+    struct ClusterBuffer readcbuf;
+    read_clusters(&readcbuf, ROOT_CLUSTER_NUMBER+1, 1); 
+    // If read properly, readcbuf should filled with 'a'
 
-    // request.buffer_size = CLUSTER_SIZE;
-    // read(request);   // Failed read due not enough buffer size
-    struct ClusterBuffer fff[5];
-    for (uint32_t i = 0; i < 5; i++)
-        for (uint32_t j = 0; j < CLUSTER_SIZE; j++)
-            fff[i].buf[j] = '\0';
+    request.buffer_size = CLUSTER_SIZE;
+    read(request);   // Failed read due not enough buffer size
+    request.buffer_size = 5*CLUSTER_SIZE;
+    read(request);   // Success read on file "daijoubu"
 
-    struct FAT32DriverRequest request2 = {
-        .buf                   = fff,
-        .name                  = "ikanaide",
-        .ext                   = "uwu",
-        .parent_cluster_number = ROOT_CLUSTER_NUMBER,
-        .buffer_size           = 0,
-    };
-    // request2.buffer_size = 5*CLUSTER_SIZE;
-    read_directory(request2);   // Success read on file "daijoubu"
-
-    while (TRUE)
-    {
-        keyboard_state_activate();
-    }
+    while (TRUE);
 }
+
+
 
 
 
