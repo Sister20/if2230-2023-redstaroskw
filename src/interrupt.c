ae829faba23d56acaba36c6b211d83f660457237
IceTeaXXD <13521024@std.stei.itb.ac.id>
2023-04-01 03:00:52 +0700

    feat: tss entry and updated gdt structure

--- src/interrupt.c
+++ src/interrupt.c
@@ -61,3 +61,13 @@ void activate_keyboard_interrupt(void) {
     out(PIC1_DATA, PIC_DISABLE_ALL_MASK ^ (1 << IRQ_KEYBOARD));
     out(PIC2_DATA, PIC_DISABLE_ALL_MASK);
 }
+
+struct TSSEntry _interrupt_tss_entry;
+
+void set_tss_kernel_current_stack(void) {
+    uint32_t stack_ptr;
+    // Reading base stack frame instead esp
+    __asm__ volatile ("mov %%ebp, %0": "=r"(stack_ptr) : /* <Empty> */);
+    // Add 8 because 4 for ret address and other 4 is for stack_ptr variable
+    _interrupt_tss_entry.esp0 = stack_ptr + 8; 
+}
--- src/lib-header/interrupt.h
+++ src/lib-header/interrupt.h
@@ -123,4 +123,20 @@ void pic_remap(void);
  */
 void main_interrupt_handler(struct CPURegister cpu, uint32_t int_number, struct InterruptStack info);
 
+extern struct TSSEntry _interrupt_tss_entry;
+
+/**
+ * TSSEntry, Task State Segment. Used when jumping back to ring 0 / kernel
+ */
+struct TSSEntry {
+    uint32_t prev_tss; // Previous TSS 
+    uint32_t esp0;     // Stack pointer to load when changing to kernel mode
+    uint32_t ss0;      // Stack segment to load when changing to kernel mode
+    // Unused variables
+    uint32_t unused_register[23];
+} __attribute__((packed));
+
+// Set kernel stack in TSS
+void set_tss_kernel_current_stack(void);
+
 #endif
