6ee99f874b96a6c3de80b03c7e8ebf86835c4ecd
IceTeaXXD <13521024@std.stei.itb.ac.id>
2023-04-02 17:01:00 +0700

    refactor: struct

--- src/gdt.c
+++ src/gdt.c
@@ -11,97 +11,97 @@ static struct GlobalDescriptorTable global_descriptor_table = {
     .table = {
         {
             // NULL DESCRIPTOR
-            .segment_low = 0,
-            .base_low = 0,
-            .base_mid = 0,
-            .type_bit = 0,
-            .non_system = 0,
-            .DPL = 0,
-            .P = 0,
-            .limit = 0,
-            .AVL = 0,
-            .L = 0,
-            .DB = 0,
-            .G = 0,
-            .base_high = 0
+            .segment_low       = 0,
+            .base_low          = 0,
+            .base_mid          = 0,
+            .type_bit          = 0,
+            .non_system        = 0,
+            .DPL               = 0,
+            .P                 = 0,
+            .limit             = 0,
+            .AVL               = 0,
+            .L                 = 0,
+            .DB                = 0,
+            .G                 = 0,
+            .base_high         = 0
         },
         {
             // KERNEL CODE
-            .segment_low = 0xFFFF,
-            .base_low = 0,
-            .base_mid = 0,
-            .type_bit = 0b1010,
-            .non_system = 1,
-            .DPL = 0,
-            .P = 1,
-            .limit = 0b1111,
-            .AVL = 0,
-            .L = 1,
-            .DB = 1,
-            .G = 1,
-            .base_high = 0
+            .segment_low       = 0xFFFF,
+            .base_low          = 0,
+            .base_mid          = 0,
+            .type_bit          = 0b1010,
+            .non_system        = 1,
+            .DPL               = 0,
+            .P                 = 1,
+            .limit             = 0b1111,
+            .AVL               = 0,
+            .L                 = 1,
+            .DB                = 1,
+            .G                 = 1,
+            .base_high         = 0
         },
         {
             // KERNEL DATA
-            .segment_low = 0xFFFF,
-            .base_low = 0,
-            .base_mid = 0,
-            .type_bit = 0b0010,
-            .non_system = 1,
-            .DPL = 0,
-            .P = 1,
-            .limit = 0b1111,
-            .AVL = 0,
-            .L = 0,
-            .DB = 1,
-            .G = 1,
-            .base_high = 0
+            .segment_low       = 0xFFFF,
+            .base_low          = 0,
+            .base_mid          = 0,
+            .type_bit          = 0b0010,
+            .non_system        = 1,
+            .DPL               = 0,
+            .P                 = 1,
+            .limit             = 0b1111,
+            .AVL               = 0,
+            .L                 = 0,
+            .DB                = 1,
+            .G                 = 1,
+            .base_high         = 0
         },
         {
             // USER MODE CODE
-            .segment_low = 0xFFFF,
-            .base_low = 0,
-            .base_mid = 0,
-            .type_bit = 0b1010,
-            .non_system = 1,
-            .DPL = 3,
-            .P = 1,
-            .limit = 0b1111,
-            .AVL = 0,
-            .L = 1,
-            .DB = 1,
-            .G = 1,
-            .base_high = 0
+            .segment_low       = 0xFFFF,
+            .base_low          = 0,
+            .base_mid          = 0,
+            .type_bit          = 0b1010,
+            .non_system        = 1,
+            .DPL               = 3,
+            .P                 = 1,
+            .limit             = 0b1111,
+            .AVL               = 0,
+            .L                 = 1,
+            .DB                = 1,
+            .G                 = 1,
+            .base_high         = 0
         },
         {
             // USER MODE DATA
-            .segment_low = 0xFFFF,
-            .base_low = 0,
-            .base_mid = 0,
-            .type_bit = 0b0010,
-            .non_system = 1,
-            .DPL = 3,
-            .P = 1,
-            .limit = 0b1111,
-            .AVL = 0,
-            .L = 0,
-            .DB = 1,
-            .G = 1,
-            .base_high = 0
+            .segment_low       = 0xFFFF,
+            .base_low          = 0,
+            .base_mid          = 0,
+            .type_bit          = 0b0010,
+            .non_system        = 1,
+            .DPL               = 3,
+            .P                 = 1,
+            .limit             = 0b1111,
+            .AVL               = 0,
+            .L                 = 0,
+            .DB                = 1,
+            .G                 = 1,
+            .base_high         = 0
         },
         {
-            .limit      = (sizeof(struct TSSEntry) & (0xF << 16)) >> 16,
+            .limit             = (sizeof(struct TSSEntry) & (0xF << 16)) >> 16,
             .segment_low       = sizeof(struct TSSEntry),
             .base_high         = 0,
             .base_mid          = 0,
             .base_low          = 0,
             .non_system        = 0,    // S bit
             .type_bit          = 0x9,
-            .DPL         = 0,    // DPL
-            .P         = 1,    // P bit
-            .DB        = 1,    // D/B bit
-            .L         = 0,    // L bit
-            .G       = 0,    // G bit
+            .DPL               = 0,    // DPL
+            .P                 = 1,    // P bit
+            .DB                = 1,    // D/B bit
+            .L                 = 0,    // L bit
+            .G                 = 0,    // G bit
         },
         {0}
     }
