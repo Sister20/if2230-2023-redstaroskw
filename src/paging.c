020662d60d55b68cfdc6500e989e0f4a093a3dca
IceTeaXXD <13521024@std.stei.itb.ac.id>
2023-04-21 22:16:09 +0700

    fix: struct, allocate page frame

--- src/paging.c
+++ src/paging.c
@@ -1,5 +1,5 @@
 #include "lib-header/paging.h"
-#include "lib-header/stdmem.h"
+// #include "lib-header/stdmem.h"
 
 __attribute__((aligned(0x1000))) struct PageDirectory _paging_kernel_page_directory = {
     .table = {
@@ -8,7 +8,6 @@ __attribute__((aligned(0x1000))) struct PageDirectory _paging_kernel_page_direct
             .flag.write_bit         = 1,
             .lower_address          = 0,
             .flag.use_pagesize_4_mb = 1,
-
         },
         [0x300] = {
             .flag.present_bit       = 1,
@@ -31,41 +30,20 @@ void update_page_directory_entry(void *physical_addr, void *virtual_addr, struct
     flush_single_tlb(virtual_addr);
 }
 
-/*
-selalu masuk interrupt pagefault - gatau knp
-HIKS - ahmad
-*/
 int8_t allocate_single_user_page_frame(void *virtual_addr) {
     // Using default QEMU config (128 MiB max memory)
     uint32_t last_physical_addr = (uint32_t) page_driver_state.last_available_physical_addr;
 
-    if (last_physical_addr < 0x8000000) {
-        // Allocate a new page frame
-        void *new_physical_addr = (void *) last_physical_addr;
-        memset(new_physical_addr, 0, PAGE_FRAME_SIZE);
-
-        // Update the page directory entry for the virtual address
-        struct PageDirectoryEntryFlag flag = {
+    update_page_directory_entry((void*) last_physical_addr, 
+    virtual_addr, 
+    (struct PageDirectoryEntryFlag) {
             .present_bit = 1,
             .write_bit = 1,
             .user_bit = 1,
-            .use_pagesize_4_mb = 1,
-        };
-        update_page_directory_entry(new_physical_addr, virtual_addr, flag);
-
-        // Update the state to the next available physical address
-        page_driver_state.last_available_physical_addr += PAGE_FRAME_SIZE;
-        return 0;
-    }
+            .use_pagesize_4_mb = 1
+        }
+    );
     return -1;
-}
-
-void update_page_directory(void *physical_addr, void *virtual_addr, struct PageDirectoryEntryFlag flag) {
-    uint32_t page_index = ((uint32_t) virtual_addr >> 22) & 0x3FF;
-
-    _paging_kernel_page_directory.table[page_index].flag          = flag;
-    _paging_kernel_page_directory.table[page_index].lower_address = ((uint32_t)physical_addr >> 22) & 0x3FF;
-    flush_single_tlb(virtual_addr);
 }
 
 void flush_single_tlb(void *virtual_addr) {
